d21438e7b152f4a22c84aea94d62353c7c6da87a
FKerim61 <fatihkerimc@gmail.com>
2024-01-01 22:29:30 +0300

    SonGüncelleme

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace degiskenler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        DateTime dt=DateTime.Now;
+        Console.WriteLine(dt);
+        Console.ReadLine();
+    }
+}
